7beccb448aca0a5b40d327bb474c7e016ca6b4d4
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2020-02-10 09:53:44 -0700

    add as.data.table for rcer_raw_* objects

--- R/as.R
+++ R/as.R
@@ -6,6 +6,7 @@
 #' exports from the REDCap API.
 #'
 #' @inheritParams base::as.data.frame
+#' @inheritParams data.table::as.data.table
 #'
 #' @examples
 #'
@@ -22,7 +23,7 @@ NULL
 #' @rdname as
 #' @method as.data.frame rcer_raw_metadata
 as.data.frame.rcer_raw_metadata <- function(x, ...) {
-  out <- read_text(x)
+  out <- read_text(x, class = 'data.frame')
   class(out) <- c("rcer_metadata", class(out))
   out
 }
@@ -31,7 +32,7 @@ as.data.frame.rcer_raw_metadata <- function(x, ...) {
 #' @rdname as
 #' @method as.data.frame rcer_raw_record
 as.data.frame.rcer_raw_record <- function(x, ...) {
-  out <- read_text(x)
+  out <- read_text(x, class = 'data.frame')
   class(out) <- c("rcer_record", class(out))
   out
 }
@@ -40,7 +41,7 @@ as.data.frame.rcer_raw_record <- function(x, ...) {
 #' @rdname as
 #' @method as.data.frame rcer_raw_project
 as.data.frame.rcer_raw_project <- function(x, ...) {
-  out <- read_text(x)
+  out <- read_text(x, class = 'data.frame')
   class(out) <- c("rcer_project_info", class(out))
   out
 }
@@ -49,7 +50,43 @@ as.data.frame.rcer_raw_project <- function(x, ...) {
 #' @rdname as
 #' @method as.data.frame rcer_raw_user
 as.data.frame.rcer_raw_user <- function(x, ...) {
-  out <- read_text(x)
+  out <- read_text(x, class = 'data.frame')
+  class(out) <- c("rcer_user", class(out))
+  out
+}
+
+#' @export
+#' @rdname as
+#' @method as.data.table rcer_raw_metadata
+as.data.table.rcer_raw_metadata <- function(x, ...) {
+  out <- read_text(x, class = 'data.table')
+  class(out) <- c("rcer_metadata", class(out))
+  out
+}
+
+#' @export
+#' @rdname as
+#' @method as.data.table rcer_raw_record
+as.data.table.rcer_raw_record <- function(x, ...) {
+  out <- read_text(x, class = 'data.table')
+  class(out) <- c("rcer_record", class(out))
+  out
+}
+
+#' @export
+#' @rdname as
+#' @method as.data.table rcer_raw_project
+as.data.table.rcer_raw_project <- function(x, ...) {
+  out <- read_text(x, class = 'data.table')
+  class(out) <- c("rcer_project_info", class(out))
+  out
+}
+
+#' @export
+#' @rdname as
+#' @method as.data.table rcer_raw_user
+as.data.table.rcer_raw_user <- function(x, ...) {
+  out <- read_text(x, class = 'data.table')
   class(out) <- c("rcer_user", class(out))
   out
 }
@@ -62,9 +99,10 @@ as.data.frame.rcer_raw_user <- function(x, ...) {
 #' user.  Used by the \code{as.data.frame} methods.
 #'
 #' @param x the raw return from the API call to REDCap
+#' @param class \code{data.frame} (default) or \code{data.table}
 #' @return a \code{data.frame}
 #'
-read_text <- function(x) {
+read_text <- function(x, class = "data.frame") {
   if (attr(x, "Content-Type")[1] == "text/csv") {
     out <- utils::read.csv(text = x, colClasses = "character")
   } else if (attr(x, "Content-Type")[1] == "application/json") {
@@ -75,5 +113,10 @@ read_text <- function(x) {
     stop(sprintf("Content-Type %s is not yet supported.",
                  attr(x, "Content-Type")[1]))
   }
+
+  if (class == "data.table") {
+    out <- data.table::as.data.table(out)
+  }
+
   out
 }
