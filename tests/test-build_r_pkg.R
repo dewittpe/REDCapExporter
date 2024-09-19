18e6f3cb1e8b6cdfc9ee59219de33cc8b24d24be
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-09-19 15:34:12 -0600

    [skip ci] update testing for build r pkg

--- tests/test-build_r_pkg.R
+++ tests/test-build_r_pkg.R
@@ -9,21 +9,23 @@ build_r_data_package(
 x <- fs::dir_tree(temppath)
 x <- unname(sapply(strsplit(x, "rcd14465"), `[`, 2))
 x[is.na(x)] <- ""
-x <- paste0("rcd14465", x)
+x <- sort(paste0("rcd14465", x))
 
 print(x)
 
 stopifnot(
   identical(
-    x, 
+    x,
     normalizePath(
-      c("rcd14465", "rcd14465/DESCRIPTION", "rcd14465/LICENSE", "rcd14465/NAMESPACE", 
-      "rcd14465/R", "rcd14465/R/datasets.R", "rcd14465/data", "rcd14465/data/metadata.rda", 
-      "rcd14465/data/project.rda", "rcd14465/data/record.rda", "rcd14465/data/user.rda", 
-      "rcd14465/inst", "rcd14465/inst/raw-data", "rcd14465/inst/raw-data/metadata.rds", 
-      "rcd14465/inst/raw-data/project.rds", "rcd14465/inst/raw-data/record.rds", 
-      "rcd14465/inst/raw-data/user.rds", "rcd14465/man", "rcd14465/man/metadata.Rd", 
-      "rcd14465/man/project.Rd", "rcd14465/man/record.Rd", "rcd14465/man/user.Rd"),
+      sort(
+        c("rcd14465", "rcd14465/DESCRIPTION", "rcd14465/LICENSE", "rcd14465/NAMESPACE",
+        "rcd14465/R", "rcd14465/R/datasets.R", "rcd14465/data", "rcd14465/data/metadata.rda",
+        "rcd14465/data/project.rda", "rcd14465/data/record.rda", "rcd14465/data/user.rda",
+        "rcd14465/inst", "rcd14465/inst/raw-data", "rcd14465/inst/raw-data/metadata.rds",
+        "rcd14465/inst/raw-data/project.rds", "rcd14465/inst/raw-data/record.rds",
+        "rcd14465/inst/raw-data/user.rds", "rcd14465/man", "rcd14465/man/metadata.Rd",
+        "rcd14465/man/project.Rd", "rcd14465/man/record.Rd", "rcd14465/man/user.Rd")
+      ),
       mustWork = FALSE
     )
   )
