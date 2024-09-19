69bd69a63fdddea9027ea2aa094d047d515a8ae7
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2024-09-19 11:38:36 -0600

    clean up vignette and add test for building r packages

---
+++ tests/test-build_r_pkg.R
@@ -0,0 +1,29 @@
+library(REDCapExporter)
+temppath <- tempdir()
+build_r_data_package(
+  x            = avs_raw_core,
+  path         = temppath,
+  author_roles = list(dewittp = c("cre", "aut")),
+)
+
+x <- fs::dir_tree(temppath)
+x <- substr(x, start = nchar(temppath) + 1, stop = nchar(x))
+x <- as.character(x)
+
+stopifnot(
+  identical(
+    x, 
+    c("rcd14465", "rcd14465/DESCRIPTION", "rcd14465/LICENSE", "rcd14465/NAMESPACE", 
+    "rcd14465/R", "rcd14465/R/datasets.R", "rcd14465/data", "rcd14465/data/metadata.rda", 
+    "rcd14465/data/project.rda", "rcd14465/data/record.rda", "rcd14465/data/user.rda", 
+    "rcd14465/inst", "rcd14465/inst/raw-data", "rcd14465/inst/raw-data/metadata.rds", 
+    "rcd14465/inst/raw-data/project.rds", "rcd14465/inst/raw-data/record.rds", 
+    "rcd14465/inst/raw-data/user.rds", "rcd14465/man", "rcd14465/man/metadata.Rd", 
+    "rcd14465/man/project.Rd", "rcd14465/man/record.Rd", "rcd14465/man/user.Rd")
+  )
+)
+
+stopifnot(
+    packageDescription(pkg = "rcd14465", lib.loc = temppath)$Package == "rcd14465",
+    grepl("\\d{4}\\.\\d{2}\\.\\d{2}\\.\\d{2}\\.\\d{2}", packageDescription(pkg = "rcd14465", lib.loc = temppath)$Version)
+    )
